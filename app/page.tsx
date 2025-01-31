a4502ee7ab003de06aafbc2574ac420ea36db899
Ryuugamine <yoerdanatan@gmail.com>
2025-01-31 21:29:19 +0700

    feat : add recomendation topics and top writers section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import { MorphingText } from "@/components/ui/morphing-text";
 import { WordRotate } from "@/components/ui/word-rotate";
 import { texts } from "@/lib/data";
 import { InteractiveHoverButton } from "@/components/ui/interactive-hover-button";
+import Image from "next/image";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
@@ -41,6 +44,91 @@ export default function Home() {
           <Card />
           <Card />
           <Card />
+        </div>
+      </section>
+
+      <section className="px-6 mt-16">
+        <div className="flex px-6 justify-between items-center">
+          <h2 className="text-2xl font-semibold">Articles</h2>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-10 col-span-3 gap-6 py-5 px-6">
+          <div className="md:col-span-7 grid grid-cols-2 gap-4">
+            <Card />
+            <Card />
+            <Card />
+          </div>
+
+          <div className="md:col-span-3">
+            <div className="px-2">
+              <h3 className="font-semibold">Recommended Topics</h3>
+              <div className="my-5">
+                <ul className="flex flex-wrap gap-2 [&>*]:px-4 [&>*]:bg-[#F2F2F2] [&>*]:flex [&>*]:items-center [&>*]:justify-center [&>*]:text-black [&>*]:py-2 [&>*]:rounded-full">
+                  <li>Javascript</li>
+                  <li>Javascript</li>
+                  <li>Programming Language</li>
+                  <li>Halo</li>
+                  <li>Halo</li>
+                  <li>Halo</li>
+                </ul>
+              </div>
+            </div>
+            <div className="px-2">
+              <h3 className="font-semibold">Top Writers</h3>
+              <div className="my-5">
+                <ul className="space-y-4">
+                  <li className="flex gap-2 items-center">
+                    <div>
+                      <Avatar className="cursor-pointer">
+                        <AvatarImage
+                          src={
+                            "https://avatars.githubusercontent.com/u/73646845?v=4"
+                          }
+                        />
+                        <AvatarFallback>{""}</AvatarFallback>
+                      </Avatar>
+                    </div>
+                    <div>
+                      <p className="font-semibold">Al Farizi Dwi Prasetyo</p>
+                      <p className="text-muted-foreground">Mahasiswa</p>
+                    </div>
+                  </li>
+                  <li className="flex gap-2 items-center">
+                    <div>
+                      <Avatar className="cursor-pointer">
+                        <AvatarImage
+                          src={
+                            "https://avatars.githubusercontent.com/u/73646845?v=4"
+                          }
+                        />
+                        <AvatarFallback>{""}</AvatarFallback>
+                      </Avatar>
+                    </div>
+                    <div>
+                      <p className="font-semibold">Al Farizi Dwi Prasetyo</p>
+                      <p className="text-muted-foreground">Mahasiswa</p>
+                    </div>
+                  </li>
+                  <li className="flex gap-2 items-center">
+                    <div>
+                      <Avatar className="cursor-pointer">
+                        <AvatarImage
+                          src={
+                            "https://avatars.githubusercontent.com/u/73646845?v=4"
+                          }
+                        />
+                        <AvatarFallback>{""}</AvatarFallback>
+                      </Avatar>
+                    </div>
+                    <div>
+                      <p className="font-semibold">Al Farizi Dwi Prasetyo</p>
+                      <p className="text-muted-foreground">Mahasiswa</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
