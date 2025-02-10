368f16ec6f6dbeaf474342292c68af9e052f1b15
Ryuugamine <yoerdanatan@gmail.com>
2025-02-10 12:10:16 +0700

    refactor : refactor db to add schema

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,11 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
 config({ path: ".env.local" });
 
-export const db = drizzle(process.env.DATABASE_URL!);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in .env.local");
+}
+
+export const db = drizzle(process.env.DATABASE_URL, { schema });
