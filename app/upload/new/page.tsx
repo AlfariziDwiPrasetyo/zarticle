36daf610b31fa4b54a43e8a5ec45186e263f7697
Ryuugamine <yoerdanatan@gmail.com>
2025-02-21 13:35:27 +0700

    add : change rich text editor to blocknote and configure it

--- app/upload/new/page.tsx
+++ app/upload/new/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-import Tiptap from "@/components/TipTap";
-import { useState } from "react";
+import dynamic from "next/dynamic";
+const BlockNote = dynamic(() => import("@/components/BlockNote"), {
+  ssr: false,
+});
 
 const page = () => {
-  const [content, setContent] = useState("");
-
-  return <Tiptap />;
+  return (
+    <div className="flex flex-col justify-center items-center bg-gray-50 gap-5">
+      <h1 className="pt-12 pb-5 text-3xl font-bold">What's on your mind ? </h1>
+      <div className="flex-grow w-screen h-screen md:p-12">
+        <form action="">
+          <BlockNote />
+        </form>
+      </div>
+    </div>
+  );
 };
 export default page;
---
+++ components/BlockNote.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import "@blocknote/core/fonts/inter.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/shadcn";
+import "@blocknote/shadcn/style.css";
+import { useEffect, useRef, useState } from "react";
+
+export default function BlockNote() {
+  const editor = useCreateBlockNote();
+  const [html, setHTML] = useState<string>("");
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const onChange = async () => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
+    timeoutRef.current = setTimeout(async () => {
+      const html = await editor.blocksToHTMLLossy(editor.document);
+      setHTML(html);
+    }, 500);
+  };
+
+  useEffect(() => {
+    onChange();
+  }, []);
+
+  console.log(html);
+
+  return (
+    <BlockNoteView
+      className="px-1 md:px-12"
+      editor={editor}
+      theme="light"
+      onChange={onChange}
+      shadCNComponents={{}}
+    />
+  );
+}
