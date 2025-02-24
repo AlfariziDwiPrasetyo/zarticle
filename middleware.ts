39860239eae03a4377c3088267de9e2d1c9dbe6c
Ryuugamine <yoerdanatan@gmail.com>
2025-02-24 11:35:47 +0700

    chore : setup middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,21 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
 
-export const { auth: middleware } = NextAuth(authConfig);
+// export const { auth: middleware } = NextAuth(authConfig);
+
+const { auth } = NextAuth(authConfig);
+
+export default auth(async function middleware(req: NextRequest) {
+  const isAuthenticated = await auth();
+
+  if (isAuthenticated) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/", req.url));
+});
+
+export const config = {
+  matcher: "/upload/:path*",
+};
